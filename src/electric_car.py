245283bd2bec95a2d34df844d8d184cdd7eeccdc
Parashuram <parashurampawar56@gmail.com>
2025-12-24 17:10:54 +0530

    modify program: [parashuram] electric car (implemented inheritance)

--- src/electric_car.py
+++ src/electric_car.py
@@ -6,15 +6,8 @@ class ElectricCar(Vehicle):
     super().__init__(vehicle_id, model, battery_percentage)
     self.seating_capacity = seating_capacity
 
-  @property
-  def battery_percentage(self):
-    return self.__battery_percentage
-  
-  @battery_percentage.setter
-  def battery_percentage(self, batter_percentage):
-    self.__battery_percentage = batter_percentage
 
-car = ElectricCar("MDC09876XD","XUV 3X0",102,5)
+car = ElectricCar("MDC09876XD","XUV 3X0",100,5)
 # car.maintainance_status = "Service done"
 # car.rental_price = 80
 # print(car.__dict__)
